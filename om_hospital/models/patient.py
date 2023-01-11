1f9b513996ad809a78fdd0b669e17513d88085a4
soks16 <sokambyj@gmail.com>
2023-01-11 19:11:41 +0100

    [UPP] to enable Tracking for fields

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -8,8 +8,8 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Patient'
 
-    name = fields.Char(string="Name")
-    age = fields.Integer(string="Age")
+    name = fields.Char(string="Name", tracking=True)
+    age = fields.Integer(string="Age", tracking=True)
     ref = fields.Char(string="Reference")
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", tracking=True)
     active = fields.Boolean(string="Active", default=True)
