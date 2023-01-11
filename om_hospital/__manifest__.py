4c51f8ae7efd16c1575fc9a7cea3bf9b1954523f
soks16 <sokambyj@gmail.com>
2023-01-11 18:31:19 +0100

    [UPP] to apply domain for menu and [ADD] file female_patient_view.xml

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient_view.xml',
+        'views/female_patient_view.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
