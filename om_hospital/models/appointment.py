ed428abb3e2553719cb4dba50e0b56a01ca2bd10
soks16 <sokambyj@gmail.com>
2023-01-12 07:26:33 +0100

    [UPP] appointment

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -9,3 +9,5 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital Appointment'
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
+    appointment_time = fields.Datetime(string="Appointment Time")
+    booking_date = fields.Date(string="Booking Date")
