57eabe0e506599ec8090c75624605ea666cfb02b
soks16 <sokambyj@gmail.com>
2023-01-13 07:40:00 +0100

    [UPP] define HTML field

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -14,6 +14,7 @@ class HospitalAppointment(models.Model):
     gender = fields.Selection(related='patient_id.gender')
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
     ref = fields.Char(string="Reference")
+    prescription = fields.Html(string="Prescription")
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
