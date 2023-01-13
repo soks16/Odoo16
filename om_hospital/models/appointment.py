e87f929777347ef397cf4062b2a4db7322caf76b
soks16 <sokambyj@gmail.com>
2023-01-13 07:30:30 +0100

    [UPP] add notebook

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -7,6 +7,7 @@ class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Appointment'
+    _rec_name = 'ref'
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
