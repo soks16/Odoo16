0c8961ebfbbf4fe64c3aa8b9105a632d4287d243
soks16 <sokamyj@gmail.com>
2023-01-30 15:34:44 +0100

    [ADD] widget color in field.Char

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -10,3 +10,6 @@ class PatientTag(models.Model):
 
     name = fields.Char(string='Name', required=True)
     active = fields.Boolean(string='Active', default=True)
+    color = fields.Integer(string="Color")
+    color_2 = fields.Char(string="Color 2")
+
